fa209c7610d9bd2ba1104107681107d4fbde1543
zhanghan <miaoihan@users.noreply.github.com>
2024-04-01 08:02:46 +0000

    perf: some text perf

--- components/navbar.js
+++ components/navbar.js
@@ -5,11 +5,11 @@ import { Disclosure } from "@headlessui/react";
 
 const Navbar = () => {
   const navigation = [
-    "Product",
-    "Features",
-    "Pricing",
-    "Company",
-    "Blog",
+    "产品",
+    "功能",
+    "价格",
+    "帮助中心",
+    "博客",
   ];
 
   return (
@@ -24,14 +24,14 @@ const Navbar = () => {
                   <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
                     <span>
                       <Image
-                        src="/img/logo.svg"
+                        src="/img/logo2.jpg"
                         alt="N"
                         width="32"
                         height="32"
                         className="w-8"
                       />
                     </span>
-                    <span>Nextly</span>
+                    <span>妙言AI</span>
                   </span>
                 </Link>
 
@@ -90,7 +90,7 @@ const Navbar = () => {
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
           <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
-              Get Started
+              进入网页端
           </Link>
 
           <ThemeChanger />
